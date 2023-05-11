445b99df3623c0ec33222a64c1c901f592333668
FrancoM <franco1.alfra@gmail.com>
2023-05-11 04:47:58 -0500

    encriptado y desencriptado hecho

--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,179 @@ import styles from '../styles/Home.module.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { useRef, useState } from 'react';
 const crypto = require("crypto-js")
-const IPFS = require('ipfs-mini')
+import { create } from 'ipfs-http-client'
+import * as fs from 'fs';
 
 export default function Home() {
 
+  const [showDownload,setShowDownload] = useState(false)
   const [file,setFile] = useState(null)
-  const [encryptedFile, setEncryptedFile] = useState(null)
-  const [decryptedFile, setDecryptedFile] = useState(null)
-  const client = new IPFS({host: 'ipfs.infura.io',port:5001,protocol:'https'})
-  let key = "llavesupersecreta123"
+  const [fileHashLocation, setfileHashLocation] = useState(null)
+  let key = "llavesupersecret"
+
+  const createIPFSClient = async () =>{
+    const ipfs = await create(
+      {
+        host:"localhost",
+        port:5001,
+      }
+    )
+
+    return ipfs
+  }
 
  const onButtonPressed = async () => {
     if (file == null){
       console.log("No hay archivo cargado")
       return
     }
-    let fr = new FileReader()
-    fr.readAsArrayBuffer(file)
+    console.log("Empieza proceso de subida")
+     let fr = new FileReader()
+     fr.readAsDataURL(file)
     fr.onloadend = () =>{
+      console.log(fr.result)
       manageOnLoadEndFR(fr.result)
     }
   }
 
-  const callEncrypt = async (argument) => {
-    const wordArray = crypto.lib.WordArray.create(argument)
-    const str = crypto.enc.Hex.stringify(wordArray)
-    let ct = crypto.AES.encrypt(str, key)
-    let ctstr = ct.toString()
-    return ctstr
+  const manageOnLoadEndFR = async (result) =>{
+    
+
+    let encriptedFile = await callEncrypt(result.replace("data:audio/mpeg;base64,", ""))
+    let hash = await addFileToIPFS(encriptedFile)
+    setfileHashLocation(hash)
+    setShowDownload(true)
   }
 
-  const manageOnLoadEndFR = async (result) =>{
-      console.log(result)
-      let encriptedFile = await callEncrypt(result)
-      console.log(encriptedFile)
-      let hash = await addFileToIPFS(encriptedFile)
-      console.log(hash)
+  const callEncrypt = async (fileReaderResult) => {
+    //const wordArray = crypto.lib.WordArray.create(fileReaderResult)
+    // const str = crypto.enc.Hex.stringify(wordArray)
 
+    console.log(fileReaderResult)
+
+    let ct = crypto.AES.encrypt(fileReaderResult, key).toString()
+
+    console.log(ct)
+
+    return ct
   }
 
   const addFileToIPFS = async (encodedStr) =>{
 
     let testBuffer = new Buffer.from(encodedStr)
-    console.log(testBuffer)
-    console.log(client)
-    client.add(testBuffer, (error, file) => {
-      if (error){
-        console.log(error)
-      }
+    let client = await createIPFSClient()
 
-      return(file)
-    })
+    console.log("cliente creado, envio pendiente")
 
+    let result = await client.add(testBuffer)
+    console.log("envio terminado")
+    if (result){
+      return result.path
+    }
+    return null
   }
 
+  const onButtonDownloadPressed = async () =>{
 
+    let encryptedStr = await ipfsDownload(fileHashLocation)
 
+    let strUrl = decrypt(encryptedStr)
 
+    saveFile(strUrl)
+  }
+
+  const ipfsDownload = async (hash) =>{
+    console.log(hash)
+
+    let client = await createIPFSClient()
+
+    const resp = await client.cat(hash)
+
+    let finalContent = []
+
+    for await (const chunk of resp) {
+      finalContent = [...finalContent, ...chunk]
+    }
+
+    const encryptedStr = Buffer.from(finalContent).toString()
+
+    return encryptedStr
+  }
+
+  const decrypt = (encryptedStr) =>{
+
+    console.log(encryptedStr)
+
+    const decrypted = crypto.AES.decrypt(encryptedStr,key)
+
+    console.log(decrypted)
+
+    var str = decrypted.toString(crypto.enc.Utf8)
+
+    console.log(str)
+
+    return str
+  }
+
+  const saveFile = async (dataURL) =>{
+
+    dataURL = "data:audio/mpeg;base64,"+dataURL
+
+    console.log(dataURL)
+
+    const fs = require('file-saver')
+    // var arrayBuffer = new Uint8Array(byteArray)
+    // var blob = new Blob([arrayBuffer])//,{type:"audio/mpeg"})
+
+    const responseURL = await fetch(dataURL)
+    
+    const blob = await responseURL.blob()
+
+    console.log(blob)
+    fs.saveAs(blob,"filename.mp3")
+  }
+
+  function wordToByteArray(word, length) {
+    var ba = [],
+      i,
+      xFF = 0xFF;
+    if (length > 0)
+      ba.push(word >>> 24);
+    if (length > 1)
+      ba.push((word >>> 16) & xFF);
+    if (length > 2)
+      ba.push((word >>> 8) & xFF);
+    if (length > 3)
+      ba.push(word & xFF);
+  
+    return ba;
+  }
+
+  function wordArrayToByteArray(wordArray, length) {
+    if (wordArray.hasOwnProperty("sigBytes") && wordArray.hasOwnProperty("words")) {
+      length = wordArray.sigBytes;
+      wordArray = wordArray.words;
+    }
+  
+    var result = [],
+      bytes,
+      i = 0;
+    while (length > 0) {
+      bytes = wordToByteArray(wordArray[i], Math.min(4, length));
+      length -= bytes.length;
+      result.push(bytes);
+      i++;
+    }
+    return [].concat.apply([], result);
+  }
+
+  const onFileSelectorChange = (target) => {
+    if (target.files) {
+      const file = target.files[0]
+      console.log(file.type)
+      setFile(file)
+    }
+  }
 
   return (
     <div className='container'>
@@ -68,30 +184,42 @@ export default function Home() {
       </h1>
 
       <div className='p-3'>
+        {showDownload ? 
+        (<div>
+          <h1>Archivo por descargar</h1>
+        </div>) : 
+        (<div>
           <label for="formFileLg" class="form-label">Large file input example</label>
           <input class="form-control form-control-sm" id="formFile" type="file"
           onChange={({target})=>{
-            if (target.files) {
-              const file = target.files[0]
-              setFile(file)
-            }
+            onFileSelectorChange(target)
           }}></input>
+        </div>)
+
+        }
       </div>
       <div className='row'>
         <div className='col'>
         </div>
         <div className='col text-center' >
-          <button type="button" class="btn btn-primary" id='buttonUpload' onClick={onButtonPressed}>Subir</button>
+          {
+            showDownload ? (
+            <button type="button" 
+                    class="btn btn-danger" 
+                    id='buttonUpload' 
+                    onClick={onButtonDownloadPressed}>Descargar
+                    </button>)
+                    :
+            (<button type="button" 
+                      class="btn btn-primary" 
+                      id='buttonDownload' 
+                      onClick={onButtonPressed}>Subir
+                      </button>)
+          }
         </div>
         <div className='col'>
         </div>
       </div>
-
-      <div>
-      {file ? (<div><a>{File.call.length}</a>
-      <a>Presente</a></div>) : <div></div>}
-      </div>
-      
     </div>
 
   )
